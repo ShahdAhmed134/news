1a05cb55a898db02b30da4dddfd3bc428f8a5f5b
Shahd Ahmed <enshahd134@gmail.com>
2024-08-12 04:33:41 +0300

    end category details.

---
+++ lib/model/api_constant.dart
@@ -0,0 +1,6 @@
+class ApiConstant {
+  static String baseUrl = 'newsapi.org';
+  static String apiSource = '/v2/top-headlines/sources';
+  static String apiNews = '/v2/everything';
+  static String apiKey = 'c81b5044792f49908da8073ac4046a19';
+}
